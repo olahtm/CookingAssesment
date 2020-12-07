ccfc6872cba6512cb96d404a9df652e833869b58
olahtm <tolah@atrisktech.com>
2020-12-07 02:45:53 +0200

    Fix deploy script.

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ docker build -t cooking-assesment .
 
 docker run --rm -v $(pwd)/apps:/build cooking-assesment cp /cooking/target/uber-CookingAssessment-1.0-SNAPSHOT.jar /build/
 
-wget https://storage.googleapis.com/whats_cookin/whats_cookin.zip
+wget https://storage.googleapis.com/whats_cookin/whats_cookin.zip -O data/cooking_data.zip
 if [ $? -eq 0 ]; then
     echo "--> Succesfully downloaded file"
 else
@@ -10,7 +10,7 @@ else
     exit 1
 fi
 
-unzip -o data/whats_cookin.zip -d data
+unzip -o data/cooking_data.zip -d data
 if [ $? -eq 0 ]; then
     echo "--> Succesfully unzip"
 else
